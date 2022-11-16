e0d76a7d5cf15d8e2f6092001cb14ee985ddacab
Maurin Schucan <maurin@schucan.ch>
2022-11-16 08:41:31 +0000

    added new endpoints and tests

--- src/main/java/ch/zli/m223/controller/EntryController.java
+++ src/main/java/ch/zli/m223/controller/EntryController.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -36,6 +39,24 @@ public class EntryController {
     @Operation(summary = "Creates a new entry.", description = "Creates a new entry and returns the newly added entry.")
     public Entry create(Entry entry) {
        return entryService.createEntry(entry);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Operation(summary = "Deletes one entry.", description = "Deletes one entry.")
+    public void delete(@PathParam("id") Long id) {
+        entryService.deleteEntry(id);
+    }
+
+    @PUT
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(
+        summary = "Updates one Entry",
+        description = "Updates one Entry"
+    )
+    public Entry update(Entry entry) {
+        return entryService.updateEntry(entry);
     }
 
 }
