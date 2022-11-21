8f9fbc941bf4c15b9ff4a60f3202d66aec49bd3e
Maurin Schucan <maurin@schucan.ch>
2022-11-21 09:52:02 +0000

    finished auth with jwt

---
+++ src/main/java/ch/zli/m223/model/User.java
@@ -0,0 +1,45 @@
+package ch.zli.m223.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+    
+    @Column(nullable = false)
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
