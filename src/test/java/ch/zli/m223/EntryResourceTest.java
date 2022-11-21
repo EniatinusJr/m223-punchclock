e56bb58b7a674db1757825b4ef6e8651ec5977c5
Maurin Schucan <maurin@schucan.ch>
2022-11-21 08:28:59 +0000

    added auth with jwt

--- src/test/java/ch/zli/m223/EntryResourceTest.java
+++ src/test/java/ch/zli/m223/EntryResourceTest.java
@@ -18,53 +18,51 @@ public class EntryResourceTest {
     @Test
     public void testIndexEndpoint() {
         given()
-          .when().get("/entries")
-          .then()
-             .statusCode(200)
-             .body(is("[]"));
+                .when().get("/entries")
+                .then()
+                .statusCode(200)
+                .body(is("[]"));
     }
 
     @Test
-  public void testDeleteEndpoint() {
-    var entry = new Entry();
-    entry.setCheckIn(LocalDateTime.now());
-    entry.setCheckOut(LocalDateTime.now());
+    public void testDeleteEndpoint() {
+        var entry = new Entry();
+        entry.setCheckIn(LocalDateTime.now());
+        entry.setCheckOut(LocalDateTime.now());
 
-    var createResponse = given()
-      .contentType(ContentType.JSON)
-      .body(entry)
-      .when().post("/entries");
+        var createResponse = given()
+                .contentType(ContentType.JSON)
+                .body(entry)
+                .when().post("/entries");
 
-    given()
-      .when().delete("/entries/" + createResponse.jsonPath().get("id"))
-      .then()
-        .statusCode(204);
-  }
+        given()
+                .when().delete("/entries/" + createResponse.jsonPath().get("id"))
+                .then()
+                .statusCode(204);
+    }
 
-  @Test
-  public void testUpdateEndpoint() {
-    var entry = new Entry();
-    entry.setCheckIn(LocalDateTime.now());
-    entry.setCheckOut(LocalDateTime.now());
+    @Test
+    public void testUpdateEndpoint() {
+        var entry = new Entry();
+        entry.setCheckIn(LocalDateTime.now());
+        entry.setCheckOut(LocalDateTime.now());
 
-    var createResponse = given()
-      .contentType(ContentType.JSON)
-      .body(entry)
-      .when().post("/entries");
+        var createResponse = given()
+                .contentType(ContentType.JSON)
+                .body(entry)
+                .when().post("/entries");
 
+        var updatedEntry = new Entry();
+        updatedEntry.setCheckIn(LocalDateTime.of(2023, 05, 1, 1, 1, 1));
+        updatedEntry.setCheckOut(LocalDateTime.of(2023, 05, 1, 1, 1, 2));
 
-    var updatedEntry = new Entry();
-    updatedEntry.setCheckIn(LocalDateTime.of(2023, 05, 1, 1, 1, 1));
-    updatedEntry.setCheckOut(LocalDateTime.of(2023, 05, 1, 1, 1, 2));
-
-    given()
-      .contentType(ContentType.JSON)
-      .body(updatedEntry)
-      .when().put("/entries/" + createResponse.jsonPath().get("id"))
-      .then()
-        .statusCode(200)
-        .body("checkIn", is("2023-05-01T01:01:01"));
-  }
-
+        given()
+                .contentType(ContentType.JSON)
+                .body(updatedEntry)
+                .when().put("/entries/" + createResponse.jsonPath().get("id"))
+                .then()
+                .statusCode(200)
+                .body("checkIn", is("2023-05-01T01:01:01"));
+    }
 
 }
